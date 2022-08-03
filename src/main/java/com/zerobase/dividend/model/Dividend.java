58323da04ac58e40351de6ff35ffcbb3abb8cbfe
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-04 03:26:11 +0900

    addCompany 시 error 해결(no Creators, like default constructor, exist)

--- src/main/java/com/zerobase/dividend/model/Dividend.java
+++ src/main/java/com/zerobase/dividend/model/Dividend.java
@@ -1,11 +1,15 @@
 package com.zerobase.dividend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Dividend {
     private LocalDateTime date;
