38963dd877b1f72eb41bead3316ab32daa279322
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-04 16:55:35 +0900

    searchCompany 구현

--- src/main/java/com/zerobase/dividend/service/CompanyService.java
+++ src/main/java/com/zerobase/dividend/service/CompanyService.java
@@ -8,6 +8,8 @@ import com.zerobase.dividend.persist.repository.CompanyRepository;
 import com.zerobase.dividend.persist.repository.DividendRepository;
 import com.zerobase.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -28,6 +30,10 @@ public class CompanyService {
             throw new RuntimeException("already exists ticker -> " + ticker);
         }
         return storeCompanyAndDividend(ticker);
+    }
+
+    public Page<CompanyEntity> getAllCompany(Pageable pageable) {
+        return companyRepository.findAll(pageable);
     }
 
     private Company storeCompanyAndDividend(String ticker) {
